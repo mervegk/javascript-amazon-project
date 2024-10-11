5ed28e94c2c34d004d4e3d71d5c8ef7acdad179d
mervegk <gekemeke123@gmail.com>
2024-10-11 17:41:42 +0300

    remove from cart

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [
+export let cart = [
   {
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2
@@ -56,4 +56,16 @@ export function updateCartQuantity(productId) {
   }, 2000);
 
   addedMessageTimeouts[productId] = timeoutId;
+}
+
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
 }
